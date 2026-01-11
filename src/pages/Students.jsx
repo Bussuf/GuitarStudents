04b275501d6d7b1067201aeade3cbfdc1536d2bb
Assaf Elad (Bussuf) <assaf.elad@gmail.com>
2026-01-11 23:51:46 +0000

    File changes

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -10,6 +10,7 @@ import NeonButton from '../components/ui/NeonButton';
 import DataTable from '../components/ui/DataTable';
 import StudentForm from '../components/students/StudentForm';
 import StudentCard from '../components/students/StudentCard';
+import InviteStudentButton from '../components/students/InviteStudentButton';
 
 export default function Students() {
   const queryClient = useQueryClient();
@@ -216,6 +217,7 @@ export default function Students() {
             </button>
           </div>
 
+          <InviteStudentButton />
           <NeonButton onClick={() => { setEditingStudent(null); setModalOpen(true); }}>
             <Plus size={20} />
             תלמיד חדש
